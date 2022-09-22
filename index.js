6e67af3938d71e9a2fd575fc6e5af8904aa1a668
tayefulislam <kazitayefulislam12@gmail.com>
2022-09-22 19:48:22 +0600

    aws

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+var cors = require('cors');
+
 // const router = require("./routers/user.router");
 const routerUser = require("./routers/user.router");
 const port = 5000 || process.env.PORT;
@@ -10,6 +12,8 @@ const port = 5000 || process.env.PORT;
 app.use("/", routerUser);
 
 app.use(express.json())
+app.use(cors());
+
 
 
 const user = JSON.parse(fs.readFileSync(__dirname + "/user.json"));
@@ -27,3 +31,10 @@ app.listen(port, () => {
 
 // export user jon file
 module.exports.users = JSON.parse(fs.readFileSync(__dirname + "/user.json"));
+
+process.on("unhandledRejection", (error) => {
+    console.log(error.name, error.message);
+    app.close(() => {
+        process.exit(1);
+    });
+});
